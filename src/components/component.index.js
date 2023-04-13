c9166c46bbec58ee14e6542dd16d835b034499a4
jermspeaks <jeremy.wong@clearlabs.com>
2023-04-13 15:35:12 -0700

    Modify component index export to take all new components

--- src/components/component.index.js
+++ src/components/component.index.js
@@ -1,6 +1,8 @@
-import HelloWorld from './HelloWorld';
+import Block from './Block';
 import Button from './Button';
-import Page from './Page';
-import Header from './Header';
+import EmptyDisplay from './EmptyDisplay';
+import Heading from './Heading';
+import Paragraph from './Paragraph';
+import Spacer from './Spacer';
 
-export default { Button, HelloWorld, Page, Header };
+export default { Block, Button, EmptyDisplay, Heading, Paragraph, Spacer };
